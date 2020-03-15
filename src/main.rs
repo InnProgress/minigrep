207b5b4e3adc643fe988a32b249cd22958129c44
InnProgress <arnoldautuch4@gmail.com>
2020-03-15 17:08:02 +0200

    fixed error printing

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ use minigrep;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Problem while running app: {}", e);
+        eprintln!("Problem while running app: {}", e);
         process::exit(1);
     }
 }
